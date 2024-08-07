e930af9caba1b584856584f659124a61ebed4149
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-08-07 17:13:42 +0300

    2.9 done

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 
-const Display = ({ name, number }) => (
-  <p>
-    {name} | {number}
-  </p>
-);
+const Display = ({ persons, search }) => {
+  const filteredPersons =
+    search.length > 0
+      ? persons.filter((p) =>
+          p.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : persons;
+
+  return (
+    <>
+      {filteredPersons.map((p) => (
+        <p key={p.name}>
+          {p.name} | {p.number}
+        </p>
+      ))}
+    </>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Waltteri Lehtinen", number: "0451234567" },
+    { name: "Waltteri Lehtinen", number: "045-1234567" },
+    { name: "Scarlett Johansson", number: "040-1234567" },
+    { name: "Sydney Sweeney", number: "050-1234567" },
+    { name: "Emilia Clarke", number: "046-1234567" },
+    { name: "Jarskibastian Lehtipihvinen", number: "055-1234567" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault(); // prevent default actions like page reload
@@ -36,12 +54,17 @@ const App = () => {
     }
   };
 
+  const handleSearchChange = (event) => setSearch(event.target.value);
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filer shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -53,10 +76,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((p) => (
-        <Display key={p.name} name={p.name} number={p.number} />
-      ))}
+      <h2>contacts</h2>
+      <Display persons={persons} search={search} />
     </div>
   );
 };
