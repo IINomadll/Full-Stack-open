5b47511cbc0e9128d35b621500df86f34aa992ed
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-11-10 12:59:12 +0200

    5.7 done

--- osa5/blogilista/frontend/src/components/Blog.jsx
+++ osa5/blogilista/frontend/src/components/Blog.jsx
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [viewAll, setViewAll] = useState(false);
+  const blogStyle = {
+    paddingLeft: 5,
+    border: "solid",
+    borderRadius: 5,
+    borderWidth: 1,
+    margin: 5,
+  };
+
+  // &ensp; is two space gap in html
+  return (
+    <div>
+      {viewAll ? (
+        <div style={blogStyle}>
+          <p>
+            {blog.title} {blog.author}&ensp;
+            <button onClick={() => setViewAll(!viewAll)}>
+              {viewAll ? "hide" : "view"}
+            </button>
+          </p>{" "}
+          <p>{blog.url}</p>
+          <p>
+            likes: {blog.likes}&ensp;<button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      ) : (
+        <div style={blogStyle}>
+          <p>
+            {blog.title} {blog.author}&ensp;
+            <button onClick={() => setViewAll(!viewAll)}>
+              {viewAll ? "hide" : "view"}
+            </button>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
