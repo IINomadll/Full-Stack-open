a1bbdcf3c17ab340aa38cf0d33fa5a9514b4ab95
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-11-25 18:06:48 +0200

    6.5 done

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -17,10 +17,14 @@ const App = () => {
     dispatch(addAnecdoteAction(content));
   };
 
+  // sorting anecdotes in descending order using spreading to maintain
+  // immutibility of original anecdotes array
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
