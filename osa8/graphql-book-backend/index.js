fa475894683820b4ba1d6aec302d51d6a2453f1d
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-12-17 16:23:03 +0200

    8.2 done

--- osa8/graphql-book-backend/index.js
+++ osa8/graphql-book-backend/index.js
@@ -86,9 +86,17 @@ let books = [
 ];
 
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -96,6 +104,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
