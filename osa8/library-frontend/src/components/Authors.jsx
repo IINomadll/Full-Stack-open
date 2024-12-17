ac27bd5a6022fc229083daaadea26f101dd4cd9e
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-12-17 23:01:21 +0200

    8.8 done

--- osa8/library-frontend/src/components/Authors.jsx
+++ osa8/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,14 @@
+import { useQuery } from "@apollo/client";
+
+import { ALL_AUTHORS } from "../queries";
+
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+  if (!props.show) return null;
+
+  const result = useQuery(ALL_AUTHORS);
+  if (result.loading) return <div>loading...</div>;
+
+  const authors = result.data.allAuthors;
 
   return (
     <div>
@@ -24,7 +30,7 @@ const Authors = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
