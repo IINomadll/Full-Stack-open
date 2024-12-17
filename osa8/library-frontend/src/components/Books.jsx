f5e7ae489f55604171a30ef55c6d46653603b7bd
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-12-17 23:16:01 +0200

    8.9 done

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,14 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null;
-  }
+import { useQuery } from "@apollo/client";
 
-  const books = [];
+import { ALL_BOOKS } from "../queries";
+
+const Books = (props) => {
+  if (!props.show) return null;
+
+  const result = useQuery(ALL_BOOKS);
+  if (result.loading) return <div>loading...</div>;
+
+  const books = result.data.allBooks;
 
   return (
     <div>
