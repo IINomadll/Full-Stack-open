495eba79e699dd9855bac8aa1faacd4fff9794d7
Waltteri Lehtinen <lehtinen.waltteri@gmail.com>
2024-12-19 21:47:39 +0200

    8.19 done

--- osa8/library-frontend/src/App.jsx
+++ osa8/library-frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Login from "./components/Login";
 
+import { ME } from "./queries";
+
 const App = () => {
   const [page, setPage] = useState("authors");
   const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const { data } = useQuery(ME, {
+    onError: (error) => console.error("Error fetching user data:", error),
+  });
+
+  useEffect(() => {
+    if (data && data.me) setUser(data.me);
+  }, [data]);
 
   return (
     <div>
--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const [currentGenre, setCurrentGenre] = useState("all");
   const result = useQuery(ALL_BOOKS);
 
   if (!props.show) return null;
@@ -10,18 +12,30 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  // flatMap ensures that genres remains as a one dimentional array
+  // Set removes any duplicate values
+  // "all" added manually at the end
+  const genres = [...new Set(books.flatMap((b) => b.genres)), "all"];
+
+  const filteredBooks =
+    currentGenre === "all"
+      ? books
+      : books.filter((b) => b.genres.includes(currentGenre));
+
   return (
     <div>
       <h2>books</h2>
-
+      in genre <strong>{currentGenre}</strong>
       <table>
-        <tbody>
+        <thead>
           <tr>
             <th></th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((book) => (
+        </thead>
+        <tbody>
+          {filteredBooks.map((book) => (
             <tr key={book.title}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
@@ -30,6 +44,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setCurrentGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
